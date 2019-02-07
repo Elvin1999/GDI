305d242a9bbb47e3a0b8497e7d35f7c15631a0ae
camalzade_elvin <camalzade_elvin@outlook.com>
2019-02-07 20:58:14 +0400

    Add project files.

---
+++ GDI/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace GDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxFigure = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButtonFill = new System.Windows.Forms.RadioButton();
+            this.radioButtonEmpty = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // comboBoxFigure
+            // 
+            this.comboBoxFigure.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFigure.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxFigure.FormattingEnabled = true;
+            this.comboBoxFigure.Location = new System.Drawing.Point(26, 29);
+            this.comboBoxFigure.Name = "comboBoxFigure";
+            this.comboBoxFigure.Size = new System.Drawing.Size(121, 27);
+            this.comboBoxFigure.TabIndex = 0;
+            this.comboBoxFigure.SelectedIndexChanged += new System.EventHandler(this.comboBoxFigure_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(171, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(326, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(506, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Color";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(226, 36);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 2;
+            this.textBoxWidth.Text = "100";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(379, 37);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 2;
+            this.textBoxHeight.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(548, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Select color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButtonFill
+            // 
+            this.radioButtonFill.AutoSize = true;
+            this.radioButtonFill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonFill.Location = new System.Drawing.Point(657, 37);
+            this.radioButtonFill.Name = "radioButtonFill";
+            this.radioButtonFill.Size = new System.Drawing.Size(50, 24);
+            this.radioButtonFill.TabIndex = 4;
+            this.radioButtonFill.TabStop = true;
+            this.radioButtonFill.Text = "Fill";
+            this.radioButtonFill.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEmpty
+            // 
+            this.radioButtonEmpty.AutoSize = true;
+            this.radioButtonEmpty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonEmpty.Location = new System.Drawing.Point(742, 36);
+            this.radioButtonEmpty.Name = "radioButtonEmpty";
+            this.radioButtonEmpty.Size = new System.Drawing.Size(77, 24);
+            this.radioButtonEmpty.TabIndex = 4;
+            this.radioButtonEmpty.TabStop = true;
+            this.radioButtonEmpty.Text = "Empty";
+            this.radioButtonEmpty.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 529);
+            this.Controls.Add(this.radioButtonEmpty);
+            this.Controls.Add(this.radioButtonFill);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxFigure);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxFigure;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButtonFill;
+        private System.Windows.Forms.RadioButton radioButtonEmpty;
+    }
+}
+
---
+++ GDI/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDI
+{
+    public partial class Form1 : Form
+    {
+        IFactory FigureFactory { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> figures = new List<string>() { "TriAngle", "Circle", "Rectangle" };
+            comboBoxFigure.Items.AddRange(figures.ToArray());
+
+
+        }
+        public bool IsSelectedComboBox { get; set; }
+        private void comboBoxFigure_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IsSelectedComboBox = true;
+
+            var item = comboBoxFigure.SelectedItem.ToString();
+            if (item == "Rectangle")
+            {
+                FigureFactory = new RectangleFactory();
+            }
+            else if (item == "Circle")
+            {
+                FigureFactory = new CircleFactory();
+            }
+            else if (item == "Triangle")
+            {
+                FigureFactory = new TriangleFactory();
+            }
+        }
+        public Color FigureColor { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            var result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FigureColor = colorDialog.Color;
+            }
+        }
+        public Point point { get; set; }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            point = new Point(e.Location.X, e.Location.Y);
+            this.Refresh();
+        }
+        int check = 0;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (comboBoxFigure.SelectedItem != null&& IsSelectedComboBox)
+            {
+                
+                using (var a = e.Graphics)
+                {
+                    if (FigureFactory.GetFigure() is TriAngle tr)
+                    {
+                        Pen pen = new Pen(FigureColor);
+                    }
+                    else if (FigureFactory.GetFigure() is Rectangle rt)
+                    {
+                        Pen pen = new Pen(FigureColor);
+                        a.DrawRectangle(pen,point.X,point.Y, int.Parse(textBoxWidth.Text)
+                            , int.Parse(textBoxHeight.Text));
+                    }
+                    else if (FigureFactory.GetFigure() is Circle cr)
+                    {
+                        Pen pen = new Pen(FigureColor);
+                    }
+                }
+            }
+        }
+
+        private void textBoxY_TextChanged(object sender, EventArgs e)
+        {
+            IsSelectedComboBox = true;
+        }
+
+        private void textBoxY_Enter(object sender, EventArgs e)
+        {
+            IsSelectedComboBox = true;
+        }
+    }
+    interface IFigure
+    {
+        Point Point { get; set; }
+        Size Size { get; set; }
+        Color Color { get; set; }
+        bool IsFilled { get; set; }
+
+    }
+    class Rectangle : IFigure
+    {
+
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+        public bool IsFilled { get; set; }
+    }
+    class Circle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+        public bool IsFilled { get; set; }
+    }
+    class TriAngle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+        public bool IsFilled { get; set; }
+    }
+    interface IFactory
+    {
+        IFigure GetFigure();
+    }
+    class TriangleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new TriAngle();
+
+        }
+    }
+    class CircleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Circle();
+        }
+    }
+    class RectangleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Rectangle();
+        }
+    }
+}
